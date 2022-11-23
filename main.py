9c173dd44b547113ea22a9374e8c430fb2b8e24b
Josué Araya García <jdag98228@gmail.com>
2022-11-22 23:24:33 -0600

    Update the options

--- main.py
+++ main.py
@@ -18,20 +18,21 @@ mockIn=""
 def calcImg(num,t1,flag):
    match num:
     case 1:
+        print("Encriptación 1: Histograma cv2\n")
         image = cv2.imread(t1)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         histogram = cv2.calcHist([gray_image], [0], None, [256], [0, 256])
         if flag: imgIn=histogram
         else: mockIn=histogram
-        
-        print("Opción 1\n")
     case 2:
+        print("Encriptación 2: Hash imagehash\n")
         if flag: imgIn=imagehash.average_hash(Image.open(t1))
         else: mockIn=imagehash.average_hash(Image.open(t1))
-        print("Opción 2\n")
     case 3:
+        print("Encriptación 3: Histograma ?\n")
         print("Opción 3\n")
     case 4:
+        print("Encriptación 4: hash ?\n")
         print("Opción 4\n")
     case _:
         print("Opción no valida\n")
@@ -55,13 +56,15 @@ def alphaAnalysis(case1,case2):
     print("El tiempo de la primera encriptación fue de "+str(endTime1 - startTime1)+"s\n")
     t2.join()
     endTime2 = time.time()
-    print("El tiempo de la primera encriptación fue de "+str(endTime2 - startTime2)+"s\n")
+    print("El tiempo de la segunda encriptación fue de "+str(endTime2 - startTime2)+"s\n")
 
     return 0
 
 
 if __name__ =="__main__":
-   
-    print(alphaAnalysis(1,2))
+    case1 = input("Digite el primer método de encriptación (Del 1 al 4): \n")
+    case2 = input("Digite el primer método de encriptación (Del 1 al 4): \n")
+    alphaAnalysis(int(case1),int(case2))
+    
     print("Done!\n")
 
